3f747a4aa294ff612d5590c04a08e8a2f27c2ed2
bellamy.dev <slyder.dev@gmail.com>
2026-02-03 09:20:02 +0100

    en cours

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
-// File: frontend/src/App.jsx
 import { useEffect, useState } from 'react'
+import Filter from './composant/Filter.jsx'
 
-const BACKEND_URL = 'http://localhost:3000/annonces' // ← Port 3002 !
+
+const BACKEND_URL = 'http://localhost:3000/annonces'
 
 export default function App() {
   const [annonces, setAnnonces] = useState([])
+  const [annoncesFiltrees, setAnnoncesFiltrees] = useState([]) // Nouvelles annonces filtrées
   const [loading, setLoading] = useState(true)
   const [error, setError] = useState(null)
 
@@ -16,6 +18,7 @@ export default function App() {
       })
       .then(data => {
         setAnnonces(data)
+        setAnnoncesFiltrees(data) // Au départ, on affiche toutes les annonces
         setLoading(false)
       })
       .catch(err => {
@@ -24,19 +27,38 @@ export default function App() {
       })
   }, [])
 
+  // Fonction pour mettre à jour les annonces filtrées
+  function handleResultatsChange(resultats) {
+    setAnnoncesFiltrees(resultats)
+  }
+
   if (loading) return <div style={{ padding: 20 }}>Chargement...</div>
   if (error) return <div style={{ padding: 20, color: 'red' }}>Erreur: {error}</div>
 
   return (
-    <div style={{ maxWidth: 1000, margin: '0 auto', padding: 24 }}>
-      <h1>Annonces</h1>
-      {annonces.map(a => (
-        <div key={a.id} style={{ border: '1px solid #ddd', padding: 16, marginBottom: 16 }}>
-          <h2>{a.titre}</h2>
-          <p>{a.description}</p>
-          <p><strong>{a.prix}€</strong> - {a.ville}</p>
-        </div>
-      ))}
-    </div>
+    <>
+      <Filter annonces={annonces} onResultatsChange={handleResultatsChange} />
+      
+      <div style={{ textAlign: 'center', maxWidth: 1000, margin: '0 auto', padding: 24}}>
+        <h1>Annonces ({annoncesFiltrees.length})</h1>
+        
+        {annoncesFiltrees.length === 0 ? (
+          <p style={{ textAlign: 'center', color: '#999', padding: '40px' }}>
+            Aucune annonce trouvée 😕
+          </p>
+        ) : (
+          annoncesFiltrees.map(a => (
+            <div key={a.id} style={{ border: '1px solid #ddd', padding: 16, marginBottom: 16, borderRadius: '8px' }}>
+              <h2>{a.titre}</h2>
+              <p>{a.description}</p>
+              <p><strong>Catégorie</strong> - {a.categorie} / {a.sous_categorie}</p>
+              <p><strong>État</strong> - {a.etat}</p>
+              <p><strong>{a.prix}€</strong> - {a.ville}</p>
+              <p><strong>Vendu par</strong> - {a.pseudo_vendeur}</p>
+            </div>
+          ))
+        )}
+      </div>
+    </>
   )
 }
